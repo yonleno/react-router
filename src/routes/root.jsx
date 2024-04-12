4385ece5bb46729118559ad88b64eaad916c23f3
Hector Parra <phector040@gmail.com>
2024-04-12 16:58:24 -0400

    Used useNavigation to add search spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -19,6 +19,13 @@ export default function Root() {
     const navigation = useNavigation();
     const submit = useSubmit();
 
+    const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
+
     useEffect(() => {
         document.getElementById("q").value = q;
       }, [q]);
@@ -31,6 +38,7 @@ export default function Root() {
           <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
@@ -43,7 +51,7 @@ export default function Root() {
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
