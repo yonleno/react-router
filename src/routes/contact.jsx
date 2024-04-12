c95cc170fe07583a4ffe1c6aa4e762fb980c010b
Hector Parra <phector040@gmail.com>
2024-04-12 17:54:29 -0400

    Throw a 404 response when a contact doesn't exist

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,7 +3,13 @@ import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
     const contact = await getContact(params.contactId);
-    return { contact };
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  return { contact };
   }
 
   export async function action({ request, params }) {
@@ -87,6 +93,9 @@ function Favorite({ contact }) {
   // yes, this is a `let` for later
   const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
     <fetcher.Form method="post">
       <button
