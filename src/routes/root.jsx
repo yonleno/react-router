5162b65c52797add0f63bb45ebc666b8cd91d39d
Hector Parra <phector040@gmail.com>
2024-04-12 11:54:07 -0400

    Added NavLink to make the sidebar show what we're looking at

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link,  useLoaderData, Form, redirect,} from "react-router-dom";
+import { Outlet, NavLink,  useLoaderData, Form, redirect,} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -45,7 +45,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -54,7 +63,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                    </NavLink>
                 </li>
               ))}
             </ul>
