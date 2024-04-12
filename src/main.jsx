382dfed4dec28448c425456a2fb81ba58808553b
Hector Parra <phector040@gmail.com>
2024-04-12 11:09:36 -0400

    Added a loader and created an edit component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import {
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {loader as contactLoader,} from "./routes/contact";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,12 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
       },
     ],
   },
