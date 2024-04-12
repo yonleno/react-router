c95fc68294b015b6c88c91f94e9038f837874293
Hector Parra <phector040@gmail.com>
2024-04-12 08:12:54 -0400

    Created root.jsx and set root as the root route's element in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
