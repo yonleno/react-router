01be0f74eecc6f993041f60ff9fcdc636a2c7422
Hector Parra <phector040@gmail.com>
2024-04-12 11:59:20 -0400

    Used useNavigation hook to add global pending ui, making app more responsive when clicking between the list

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink,  useLoaderData, Form, redirect,} from "react-router-dom";
+import { Outlet, NavLink,  useLoaderData, Form, redirect, useNavigation,} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -13,6 +13,7 @@ export async function loader() {
 
 export default function Root() {
     const { contacts } = useLoaderData();
+    const navigation = useNavigation();
     return (
       <>
         <div id="sidebar">
@@ -74,7 +75,9 @@ export default function Root() {
           )}
           </nav>
         </div>
-        <div id="detail">
+        <div id="detail" className={
+          navigation.state === "loading" ? "loading" : ""
+        }>
         <Outlet />
         </div>
       </>
